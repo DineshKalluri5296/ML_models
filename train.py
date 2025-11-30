c04086b0ff16498c01e0e87142c88224271f35ff
DineshKalluri5296 <dinesh.kalluri@outlook.com>
2025-11-30 18:56:50 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,20 +1,15 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-import joblib
+
 
 df = pd.read_csv("Salary_Data.csv")
 
 X = df[["YearsExperience"]]
 y = df["Salary"]
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-joblib.dump(model, "model.pkl")
-
-print("Linear Regression model trained and saved as model.pkl")
