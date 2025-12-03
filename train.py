9ea21a576da4a8af7bb4e1513aecced143caf8d7
DineshKalluri5296 <dinesh.kalluri@outlook.com>
2025-12-03 09:00:27 +0530

    Update train.py

--- train.py
+++ train.py
@@ -5,22 +5,7 @@ import seaborn as sns
 import joblib
 
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-
-df= pd.read_csv("kyphosis.csv")
-
-df.isnull().sum()
-df.columns
-
-x=df[['Age', 'Number', 'Start']]
-y=df["Kyphosis"]
-
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
-
-
-model=LogisticRegression()
-
-model.fit(x_train,y_train)
+from sklearn.linear_model import LinearRegression
 
 joblib.dump(model, "kyphosis.pkl")
 print("✅ Model saved as kyphosis.pkl")
